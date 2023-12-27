7ae8aa798dee88505057592f7f3282f5020a6e8e
Bibek <vivekregmi83@gmail.com>
2023-12-27 09:15:22 +0545

    policy readded with model(Project) for the authorization

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -3,6 +3,7 @@
 namespace App\Policies;
 
 use App\User;
+use App\Project;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class ProjectPolicy
@@ -10,11 +11,72 @@ class ProjectPolicy
     use HandlesAuthorization;
 
     /**
-     * Create a new policy instance.
+     * Determine whether the user can view the project.
      *
-     * @return void
+     * @param  \App\User  $user
+     * @param  \App\Project  $project
+     * @return mixed
      */
-    public function __construct()
+    public function view(User $user, Project $project)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create projects.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the project.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Project  $project
+     * @return mixed
+     */
+    public function update(User $user, Project $project)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the project.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Project  $project
+     * @return mixed
+     */
+    public function delete(User $user, Project $project)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the project.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Project  $project
+     * @return mixed
+     */
+    public function restore(User $user, Project $project)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the project.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Project  $project
+     * @return mixed
+     */
+    public function forceDelete(User $user, Project $project)
     {
         //
     }
